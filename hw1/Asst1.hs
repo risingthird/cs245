6889fac2c95e445b1a50fbae0e7a102252e4b7f9
jiaping_wang <wangjp9802@gmail.com>
2018-09-17 08:27:17 -0400

    Update Asst1.hs

--- hw1/Asst1.hs
+++ hw1/Asst1.hs
@@ -41,7 +41,7 @@ greet arg = "Hi " ++ arg
 greet2 :: String -> String
 greet2 arg
   | (null arg) == True = "Hi there"
-  | otherwise  = "Hi " ++ arg
+  | otherwise          = "Hi " ++ arg
 
 -- problem 13
 {-
@@ -59,42 +59,45 @@ makeAs 3
 "aaa"
 -}
 
-
+-- makeAs n will return "a...a" a total of n number of "a"
 makeAs 0 = ""
 makeAs n = "a" ++ makeAs (n-1)
 
+-- twiceAs n will return "a...a" a total of 2n number of "a"
 twiceAs 0 = ""
 twiceAs n = makeAs n ++ makeAs n
 
+-- countDown will return "n n-1 n-2 ... 1 "
 countDown 1 = "1 "
 countDown n
   | n < 1     = "Too low"
   | otherwise = show n ++ " " ++ countDown (n-1)
 
+-- countUp n will return "1 2 3 4 5 ... n "
 countUp 1 = "1 "
 countUp n
   | n < 1     = "Too low"
   | otherwise = countUp (n-1) ++ show n ++ " "
 
--- Don't know whether this is ok
+-- triangle 1 return 1
+-- triangle 2 return 1 + 2
+-- triangle 3 return 1 + 2 + 3
+-- and so on so forth
 triangle n
   | n < 1     = error "Invalid Argument"
   | otherwise = sum[1..n]
 
--- divides :: Integer -> Integer -> Bool
--- divides m n
---   | n `mod` m == 0 = True
---   | otherwise = False
-
+-- divides m n return true if n can be divided by m
 divides m n = n `mod` m == 0
 
+-- hasNoDivisorsLessThan n d will return true if n has any divisor larger than 1
+-- and less than d
 hasNoDivisorsLessThan n 2 = True
 hasNoDivisorsLessThan n d
-  | d < 1     = False
-  | d == 1    = False
+  | d < 2     = False
   | otherwise = (not ((d-1) `divides` n)) && hasNoDivisorsLessThan n (d-1)
 
---guess it from the syntax from Python, and it works well
+-- Return true if m is a prime
 isPrime m
   | m < 1     = False
   | otherwise = hasNoDivisorsLessThan m m
